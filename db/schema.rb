d87029ad8d8839f20e55fd17a68f2d970604257c
takahiro-ueda <pt25ue@gmail.com>
2020-03-05 21:43:10 +0900

    .固定費科目機能を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_044335) do
+ActiveRecord::Schema.define(version: 2020_03_05_113545) do
+
+  create_table "fixedcosts", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "incomes", force: :cascade do |t|
     t.string "name", null: false
