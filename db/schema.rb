52845e255fbd5c9524301b9a33dfbaf611bc48cf
takahiro-ueda <pt25ue@gmail.com>
2020-03-24 10:48:01 +0900

    サンプル

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_032009) do
+ActiveRecord::Schema.define(version: 2020_03_09_080532) do
 
   create_table "fixedcosts", force: :cascade do |t|
     t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "income_values", force: :cascade do |t|
+    t.integer "income_id", null: false
+    t.date "year_month"
+    t.integer "value"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
